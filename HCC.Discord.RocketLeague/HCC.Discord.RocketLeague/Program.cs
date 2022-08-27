72f8a68bc8ce972e3494f9253a3ce4f4710869bb
Landon Petzoldt <16811723+clurdish@users.noreply.github.com>
2022-08-27 13:47:32 -0500

    Scrape upcoming tournament times

---
+++ HCC.Discord.RocketLeague/HCC.Discord.RocketLeague/Program.cs
@@ -0,0 +1,41 @@
+﻿using HCC.Discord.RocketLeague;
+
+Console.WriteLine("Rocket League tournament times. Press any key to stop.\n");
+
+var cts = new CancellationTokenSource();
+var readLineTask = ReadLine();
+var printTimesTask = PrintTimes(cts.Token);
+
+await Task.WhenAny(readLineTask, printTimesTask);
+cts.Cancel();
+
+
+async Task ReadLine()
+{
+    while (!Console.KeyAvailable)
+    {
+        await Task.Delay(100);
+    }
+}
+
+async Task PrintTimes(CancellationToken cancellationToken)
+{
+    var rlgService = new RocketLeagueService();
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        var tournamentInfoList = await rlgService.GetTouramentTimes();
+        
+        foreach (var t in tournamentInfoList)
+        {
+            Console.WriteLine($"{t.MatchType}: {t.StartTime?.ToLocalTime()}");
+        }
+        Console.WriteLine();
+
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return;
+        }
+
+        await Task.Delay(5000);
+    }
+}
---
+++ HCC.Discord.RocketLeague/HCC.Discord.RocketLeague/RocketLeagueService.cs
@@ -0,0 +1,45 @@
+﻿using HtmlAgilityPack;
+
+namespace HCC.Discord.RocketLeague;
+
+internal class RocketLeagueService
+{
+    public async Task<List<TournamentInfo>> GetTouramentTimes()
+    {
+        using var client = new HttpClient();
+        var uri = new Uri("https://rocket-league.com/tournaments");
+        var httpResponse = await client.GetAsync(uri);
+        httpResponse.EnsureSuccessStatusCode();
+        var htmlContent = await httpResponse.Content.ReadAsStringAsync();
+
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(htmlContent);
+        var tournamentItemNodes = htmlDoc.DocumentNode.SelectNodes("//li[@class='rlg-tournament__item']");
+        var tournamentInfoModels = tournamentItemNodes.Select(node =>
+        {
+            var itemInfoXPath = $"{node.XPath}//div[@class='rlg-tournament__item-info']";
+
+            var matchTypeNode = node.SelectSingleNode($"{itemInfoXPath}/h2");
+            var matchType = matchTypeNode.GetDirectInnerText();
+
+            var startTimeNode = node.SelectSingleNode($"{itemInfoXPath}//span[@class='rlg-tournament__item-timer']");
+            var startTimeString = startTimeNode.GetAttributeValue("data-time", "");
+            var successfullyParsedTime = DateTime.TryParse(startTimeString, out var startTime);
+            DateTime.SpecifyKind(startTime, DateTimeKind.Utc);
+
+            return new TournamentInfo
+            {
+                MatchType = matchType,
+                StartTime = successfullyParsedTime ? startTime : null,
+            };
+        }).ToList();
+
+        return tournamentInfoModels;
+    }
+}
+
+internal class TournamentInfo
+{
+    public string? MatchType { get; set; }
+    public DateTime? StartTime { get; set; }
+}
